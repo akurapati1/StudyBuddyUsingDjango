5e6abf772fa64bf7358a892b3102f5ce85f3dd18
Abhilash Kurapati <abhilash.kurapati@slu.edu>
2025-03-11 19:43:50 -0500

    Learned about python tags used in templates

--- studybuddy/base/views.py
+++ studybuddy/base/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 # Create your views here.
 
+
+rooms = [
+    {'id' :1, 'name': "Lets learn Django"},
+    {'id' :2, 'name': "Lets learn Sprintgboot"},
+    {'id' :3, 'name': "Lets learn Gen AI"},
+]
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 def room(request):
     return render(request, 'room.html')
